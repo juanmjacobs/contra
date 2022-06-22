a525dc45555e7e2d9199d633b52f8daae7517046
Juan Jacobs <juanmjacobs@gmail.com>
2022-06-22 13:33:41 -0300

    add players

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useCallback } from "react";
+import { ListGroup, Form, Row, Col, Button } from "react-bootstrap";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [newPlayerName, setNewPlayerName] = useState("");
+
+  const addNewPlayer = (e) => {
+    if(newPlayerName) {
+      const newPlayers = players.concat({ name: newPlayerName, points: 0 });
+      setPlayers(newPlayers);
+      setNewPlayerName("");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row style={{padding: 15}}>
+        <Col xs={4}>
+          </Col>
+        <Col xs="auto">
+            <Form.Control placeholder="Agregar jugador" value={newPlayerName} onChange={e => setNewPlayerName(e.target.value)}/>
+        </Col>
+        <Col xs="auto">
+          <Button variant="primary" type="submit" onClick={addNewPlayer} className="mb-2"> + </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12}>
+          <ListGroup>
+            {players.map(({ name }) => (
+              <ListGroup.Item>{name}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+      </Row>
+      <Row className="justify-content-center" pullRight style={{margin: 15}}>
+        <Col xs="auto">
+          <Button variant="primary" type="submit" onClick={addNewPlayer} className="mb-2" disabled={players.length < 2}> Estamos todos! </Button>
+        </Col>
+      </Row>
     </div>
   );
 }
