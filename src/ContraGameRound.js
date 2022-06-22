f9be193fb3c6ac6a34eeb452b9b20e2b40af5ab7
Juan Jacobs <juanmjacobs@gmail.com>
2022-06-22 14:54:10 -0300

    rounds

--- src/ContraGameRound.js
+++ src/ContraGameRound.js
@@ -14,11 +14,36 @@ function ChoiceCard({ choiceOption }) {
     </ListGroup>
   )
 }
+const generateInitialRounds = (players) => {
+  return players.map((player, i) => {
+    const round = {
+      playerA: player,
+      playerB: players[i + 1] || players[0], //El ultimo juega contra el primero
+      playerAScore: 0,
+      playerBScore: 0,
+      played: false,
+    }
+    return round;
+  });
+}
+
 export function ContraGameRound({ players }) {
   const [currentChoice, setCurrentChoice] = useState(null);
+  const initialRounds = generateInitialRounds(players);
+  const [rounds, setRounds] = useState(initialRounds);
   const setRandomChoice = () => {
     setCurrentChoice(_.sample(choices))
   }
+  const resetRounds = () => {
+    setRounds(initialRounds)
+  }
+  const setNextRound = () => {
+    const updatedRounds = rounds.map(it => it === currentRound ? { currentRound, played: true }: it);
+    setRounds(updatedRounds);
+  }
+
+  const currentRound = _(rounds).find(it => !it.played);
+  
   useEffect(setRandomChoice)
   if(!currentChoice) return null;
   return (
@@ -34,7 +59,18 @@ export function ContraGameRound({ players }) {
       </Col>
 
       <Col xs={9}>
+        <Row style={{ display: "flex", justifyContent: "center", marginTop: 15 }}>
+          {
+            currentRound && (
+            <div>
+              {currentRound.playerA.name} vs {currentRound.playerB.name}
+            </div>
+          )}
+        </Row>
 
+        <Row style={{ display: "flex", justifyContent: "center", marginTop: 15 }}>
+          <Button variant="default" onClick={setRandomChoice}><i className="fa fa-refresh"></i></Button>
+        </Row>
         <Row>
           <Col xs={6}>
             <ChoiceCard choiceOption={currentChoice.firstChoice}/>
@@ -43,9 +79,8 @@ export function ContraGameRound({ players }) {
             <ChoiceCard choiceOption={currentChoice.secondChoice}/>
           </Col>
         </Row>
-        <Row style={{ display: "flex", justifyContent: "center", marginTop: 15 }}>
-
-          <Button onClick={setRandomChoice}><i className="fa fa-refresh"></i></Button>
+        <Row style={{ display: "flex", justifyContent: "right", margin: 15 }}>
+          <Button onClick={() => !currentRound ? resetRounds() : setNextRound()}>{ !currentRound ? "Comenzar" : "Siguiente turno" }</Button>
         </Row>
       </Col>
     </Row>
